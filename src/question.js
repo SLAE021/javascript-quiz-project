12d29c0ff364a73952f3d9d88710d664cdd111f8
Samuel Angulo <samuelangulo70@gmail.com>
2024-09-02 17:48:29 +0200

    Organize data and functionality using classes: Question class --- done

--- src/question.js
+++ src/question.js
@@ -6,14 +6,38 @@ class Question {
         this.answer = answer;
         this.difficulty = difficulty;
     }
-       
-        
-    
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+
+    shuffleChoices() {
+        let array = this.choices
+        for (let i = 0; i < array.length; i++) {
+            // for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+            return array;
+        }
+
+     
+
+
+    }
 
     // 2. shuffleChoices()
-}
+    /*
+
+    shuffleChoices() {
+            let array = this.choices
+        // for (let i = 0; i < array.length; i++) {
+            for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+            return array;
+        }
+    
+
+    */
+
 
 
 /*
