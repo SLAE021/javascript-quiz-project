a2535ff346aaeb382fb70f86a8889e30cef5aa7f
Samuel Angulo <samuelangulo70@gmail.com>
2024-09-02 18:28:25 +0200

    Day uno completado

--- src/question.js
+++ src/question.js
@@ -9,59 +9,13 @@ class Question {
 
     shuffleChoices() {
         let array = this.choices
-        for (let i = 0; i < array.length; i++) {
+            for (let i = 0; i < array.length; i++) {
             // for (let i = array.length - 1; i > 0; i--) {
                 const j = Math.floor(Math.random() * (i + 1));
                 [array[i], array[j]] = [array[j], array[i]];
             }
             return array;
         }
-
-     
-
-
+   
     }
 
-    // 2. shuffleChoices()
-    /*
-
-    shuffleChoices() {
-            let array = this.choices
-        // for (let i = 0; i < array.length; i++) {
-            for (let i = array.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                [array[i], array[j]] = [array[j], array[i]];
-            }
-            return array;
-        }
-    
-
-    */
-
-
-
-/*
-
-class Hero {
-
-  constructor(parameterName, parameterIdentity) { // dynamic parameters can be changed when instanced
-    // 'constructor' works as a reserved word for each class
-    // 'constructor' definews all the properties that the class will have
-
-    this.name = parameterName;
-    this.identity = parameterIdentity;
-    this.isVillain = false; // static parameters remain the same
-  }
-
-  // methods = here all the methods that has the class
-  revealSecretIdentity() {
-    return `i'm ${this.name} and my secret identity is ${this.identity}`;
-  }
-
-  becomeVillain() {
-    this.isVillain = true; // modifies an object propertie
-    return `i'm ${this.name} and now i'm a villain`
-  }
-}
-
-*/
--- src/quiz.js
+++ src/quiz.js
@@ -1,16 +1,45 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  // returns the question from the questions array at the position of currentQuestionIndex
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 4. shuffleQuestions()
+  // should increment the currentQuestionIndex by 1
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
+  shuffleQuestions() {
+    let array = this.questions;
+    for (let i = 0; i < array.length; i++) {
+      // for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
 
-    // 5. checkAnswer(answer)
+  // should increase correctAnswers by 1 when called with a correct answer for the current question
+  checkAnswer(answer) {
+    if (this.getQuestion().answer === answer) {
+      this.correctAnswers += 1; // this.correctAnswers = this.correctAnswers + 1
+    }
+  }
 
-    // 6. hasEnded()
-    
-} 
+  // should return false when currentQuestionIndex is less than the questions array length
+  // should return true when currentQuestionIndex is equal to the questions array length
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if ((this.currentQuestionIndex = this.questions.length)) {
+      return true;
+    }
+  }
+}
