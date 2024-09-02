2e1d45d0b974c6afbcc2814d65ed487de2f60a94
Samuel Angulo <samuelangulo70@gmail.com>
2024-09-02 18:39:01 +0200

    se actualizaron los espacios

--- src/question.js
+++ src/question.js
@@ -1,21 +1,18 @@
 class Question {
-    constructor (text, choices, answer, difficulty) {
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
 
-        this.text = text;
-        this.choices = choices;
-        this.answer = answer;
-        this.difficulty = difficulty;
+  shuffleChoices() {
+    let array = this.choices;
+    for (let i = 0; i < array.length; i++) {
+      // for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
-
-    shuffleChoices() {
-        let array = this.choices
-            for (let i = 0; i < array.length; i++) {
-            // for (let i = array.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                [array[i], array[j]] = [array[j], array[i]];
-            }
-            return array;
-        }
-   
-    }
-
+    return array;
+  }
+}
